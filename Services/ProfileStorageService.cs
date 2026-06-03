d9396102f6aff4eabd2dec2e647f399f2f8b24a2
StillMino <trogoros24@gmail.com>
2026-06-04 00:54:05 +0500

    Expand profile storage operations

--- Services/ProfileStorageService.cs
+++ Services/ProfileStorageService.cs
@@ -1,13 +1,16 @@
+using SpecStudioParser.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace SpecStudioParser.Services
 {
     public static class ProfileStorageService
     {
         public const string DefaultProfilesFolderName = "SpecStudioProfiles";
+        public const string DefaultProfileFileName = "Новый_профиль.xml";
 
         public static string GetDefaultProfilesPath()
         {
@@ -44,7 +47,7 @@ namespace SpecStudioParser.Services
         {
             if (string.IsNullOrWhiteSpace(fileName))
             {
-                return "Новый_профиль.xml";
+                return DefaultProfileFileName;
             }
 
             return fileName.EndsWith(".xml", StringComparison.OrdinalIgnoreCase)
@@ -56,6 +59,33 @@ namespace SpecStudioParser.Services
         {
             EnsureProfilesFolder(profilesPath);
             return Path.Combine(profilesPath, NormalizeXmlFileName(fileName));
+        }
+
+        public static ReportProfile LoadProfile(string profilesPath, string fileName)
+        {
+            var profilePath = GetProfilePath(profilesPath, fileName);
+            return MscsXmlService.LoadFromMscsXml(profilePath);
+        }
+
+        public static string SaveProfile(string profilesPath, string fileName, XDocument document)
+        {
+            var normalizedFileName = NormalizeXmlFileName(fileName);
+            var profilePath = GetProfilePath(profilesPath, normalizedFileName);
+            document.Save(profilePath);
+            return normalizedFileName;
+        }
+
+        public static string CopyProfileIntoStorage(string sourcePath, string profilesPath)
+        {
+            if (string.IsNullOrWhiteSpace(sourcePath) || !File.Exists(sourcePath))
+            {
+                throw new FileNotFoundException("XML-файл профиля не найден.", sourcePath);
+            }
+
+            var fileName = NormalizeXmlFileName(Path.GetFileName(sourcePath));
+            var destinationPath = GetProfilePath(profilesPath, fileName);
+            File.Copy(sourcePath, destinationPath, true);
+            return fileName;
         }
     }
 }
