ca17731ed4072fe0c64a9d447be8c0ca6ec7580f
StillMino <trogoros24@gmail.com>
2026-06-07 00:16:42 +0500

    Avoid duplicate root filter move handlers

--- Views/SpecificationSettingsWindow.axaml.cs
+++ Views/SpecificationSettingsWindow.axaml.cs
@@ -189,37 +189,6 @@ namespace SpecStudioParser.Views
             }
         }
 
-        private void MoveRootFilterItemUpClick(object sender, RoutedEventArgs e)
-        {
-            MoveRootFilterItem(sender, -1);
-        }
-
-        private void MoveRootFilterItemDownClick(object sender, RoutedEventArgs e)
-        {
-            MoveRootFilterItem(sender, 1);
-        }
-
-        private void MoveRootFilterItem(object sender, int direction)
-        {
-            if (sender is not Button button ||
-                button.DataContext is not FilterRootItem item ||
-                DataContext is not MainWindowViewModel viewModel ||
-                viewModel.SelectedDataset == null)
-            {
-                return;
-            }
-
-            var items = viewModel.SelectedDataset.RootFilterItems;
-            var oldIndex = items.IndexOf(item);
-            var newIndex = oldIndex + direction;
-            if (oldIndex < 0 || newIndex < 0 || newIndex >= items.Count)
-            {
-                return;
-            }
-
-            items.Move(oldIndex, newIndex);
-        }
-
         private void CloseWindowClick(object sender, RoutedEventArgs e)
         {
             this.Close();
