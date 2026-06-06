fc07610cca70cadd0090fb6dd869549009cfbbac
StillMino <trogoros24@gmail.com>
2026-06-07 00:07:40 +0500

    Handle copy requests in CADLib parameter browser

--- Views/CadLibParameterBrowserWindow.axaml.cs
+++ Views/CadLibParameterBrowserWindow.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using SpecStudioParser.CadLib;
+using System;
 
 namespace SpecStudioParser.Views
 {
@@ -14,12 +15,27 @@ namespace SpecStudioParser.Views
             if (DataContext is CadLibParameterBrowserViewModel viewModel)
             {
                 viewModel.RequestClose += () => this.Close();
+                viewModel.RequestCopy += CopyToClipboard;
             }
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private async void CopyToClipboard(string text)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(text) && Clipboard != null)
+                {
+                    await Clipboard.SetTextAsync(text);
+                }
+            }
+            catch
+            {
+            }
         }
     }
 }
