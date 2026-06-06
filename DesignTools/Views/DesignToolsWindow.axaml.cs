03ea89102b7f74cb99ddb0399091f32eca9b8e4c
StillMino <trogoros24@gmail.com>
2026-06-07 02:09:21 +0500

    Add design tools window code behind

---
+++ DesignTools/Views/DesignToolsWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SpecStudioParser.DesignTools.Views
+{
+    public partial class DesignToolsWindow : Window
+    {
+        public DesignToolsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
