8fef037c0dab03e6ac0a4f32c3595967023e3b56
StillMino <trogoros24@gmail.com>
2026-06-27 02:28:46 +0500

    fix: CS0103 RXObject — add using Teigha.Runtime

--- attributeexportpro/services/attributescannerservice.cs
+++ attributeexportpro/services/attributescannerservice.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Teigha.DatabaseServices;
+using Teigha.Runtime;
 using HostMgd.ApplicationServices;
 using CadApp = HostMgd.ApplicationServices.Application;
 using SpecStudioParser.AttributeExportPro.Models;
